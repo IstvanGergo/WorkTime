5cdcafea1c22a264535776c69ba7e6fad04c9d8a
IstvanGergo <istvangergo96@gmail.com>
2023-04-09 16:29:11 +0200

    Namespace changes

--- Platforms/MacCatalyst/AppDelegate.cs
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,9 @@
 ﻿using Foundation;
 
-namespace WorkTime;
+namespace WorkTime.Platforms.MacCatalyst;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
