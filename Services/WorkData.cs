c807273347bfc94333bd5d72355e1725d112fa99
IstvanGergo <istvangergo96@gmail.com>
2023-04-20 21:30:59 +0200

    The databasepath now works as intended

--- Services/WorkData.cs
+++ Services/WorkData.cs
@@ -1,37 +1,31 @@
-﻿using CommunityToolkit.Mvvm.Input;
-
-namespace WorkTime.Services;
+﻿namespace WorkTime.Services;
 
 public class WorkData
 {
+    /*THESE TWO STRINGS ARE NEEDED FOR THE DB TO BE LOADED INTO APP DATA DIRECTORY*/
     public static string DatabaseName = "WorkTimeDB.db";
-    public static string DatabasePath
-    {
-        get
-        {
+    public static string DatabasePath = Path.Combine(FileSystem.AppDataDirectory, DatabaseName);
 
-            var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            string path = Path.Combine(basePath + @"\" + DatabaseName);
-            return $"Data Source={path}";
-        }
-    }
-    static async Task<string> LoadMauiAsset()
+    /*THIS STRING IS FOR THE SQLITE CONNECTION*/
+    public static string DBpath = $"Data Source={DatabasePath}";
+    static void LoadDatabase()
     {
-        using var stream = await FileSystem.OpenAppPackageFileAsync("WorkTimeDB.db");
-        using var reader = new StreamReader(stream);
-        var contents = reader.ReadToEnd();
-        return contents;
+        Task<Stream> task = FileSystem.OpenAppPackageFileAsync($"{DatabaseName}");
+        var stream = task.Result;
+        using MemoryStream memoryStream = new();
+        stream.CopyTo(memoryStream);
+        File.WriteAllBytes(DatabasePath, memoryStream.ToArray());
     }
 
     List<WorkTimeEntry> entries = new();
     public WorkData()
     {
-        LoadMauiAsset();
+        LoadDatabase();
 
     }
     public static void CreateTable()
     {
-        using var conn = new SqliteConnection(DatabasePath);
+        using var conn = new SqliteConnection(DBpath);
         SqliteCommand command = conn.CreateCommand();
         conn.Open();
         command.CommandText = @"
@@ -44,35 +38,36 @@ public class WorkData
         );";
         command.ExecuteNonQuery();
     }
-    public static void InsertData(DateTime date, TimeSpan start, TimeSpan end, long distance) // Check if the Data is already in the database. If it is, ask the user if he wants to update said data
+    public static void InsertData(DateTime date, TimeSpan start, TimeSpan end, long distance) // Datatype Mismatch, i don't know why yet.
     {
-        using var conn = new SqliteConnection(DatabasePath);
+        using var conn = new SqliteConnection(DBpath);
         conn.Open();
         SqliteCommand sqlite_cmd = conn.CreateCommand();
-        string date_to_insert = date.ToString("s").Remove(10); //
+        string date_to_insert = date.ToString("s").Remove(10); //This way the format will be YYYY:MM:DD
         sqlite_cmd.CommandText = $"SELECT * FROM WorkTime WHERE Date = {date_to_insert}";
         TimeSpan length = end - start;
-        string time = string.Format("{0:00}:{1:00}", length.Hours, length.Minutes); //
-        string Start = string.Format("{0:00}:{1:00}", start.Hours, start.Minutes);
-        string End = string.Format("{0:00}:{1:00}", end.Hours, end.Minutes);
+        string time = string.Format("{0:00}:{1:00}", length.Hours, length.Minutes); //this way the format will be HH:MM
+        string Start = string.Format("{0:00}:{1:00}", start.Hours, start.Minutes); //this way the format will be HH:MM
+        string End = string.Format("{0:00}:{1:00}", end.Hours, end.Minutes); //this way the format will be HH:MM
         if (length < TimeSpan.Zero)
         {
             Shell.Current.DisplayAlert("HIBA!", "A két idő különbsége nem lehet kisebb mint 0!", "OK");
         }
         if (sqlite_cmd.ExecuteScalar() == null)
         {
-            sqlite_cmd.CommandText = $"INSERT INTO WorkTime (Date, Start, End, Time, Distance) VALUES ('{date_to_insert}','{Start}', '{End}', '{time}', {distance})";
+
+            sqlite_cmd.CommandText = $"INSERT INTO WorkTime VALUES ('{date_to_insert}','{Start}', '{End}', '{time}', {(int)distance})";
             sqlite_cmd.ExecuteNonQuery();
         }
         else
         {
-            sqlite_cmd.CommandText = $"UPDATE WorkTime Set Start = {start}, End = {end}, Time = {time}, Distance = {distance} WHERE Date = {date_to_insert}";
+            sqlite_cmd.CommandText = $"UPDATE WorkTime Set Start = {Start}, End = {End}, Time = {time}, Distance = {(int)distance} WHERE Date = {date_to_insert}";
             sqlite_cmd.ExecuteNonQuery();
         }
     }
     public List<WorkTimeEntry> GetItems(DateTime start, DateTime end) // List all records between the two dates
     {
-        using var conn = new SqliteConnection(DatabasePath);
+        using var conn = new SqliteConnection(DBpath);
         conn.Open();
         string s = start.ToString("s").Remove(10);
         string e = end.ToString("s").Remove(10);
@@ -83,9 +78,11 @@ public class WorkData
             reader =  sqlite_cmd.ExecuteReader();
             while (reader.Read())
             {
+                string date = reader.GetString(1);
+                date = date.Remove(10);
                 WorkTimeEntry data = new()
                 {
-                    Date = (string)reader[1],
+                    Date = date,
                     Start = (string)reader[2],
                     End = (string)reader[3],
                     Time = (string)reader[4],
@@ -100,15 +97,15 @@ public class WorkData
         }
         return entries;
     } 
-    public static void DeleteData(int id)
+    public static void DeleteData(int id) //THIS METHOD SHOULD GET A DATE INSTEAD OF ROWID
     {
-        using var conn = new SqliteConnection(DatabasePath);
+        using var conn = new SqliteConnection(DBpath);
         conn.Open();
-        SqliteCommand sqlite_cmd = new($"DELETE FROM WorkTime WHERE ROWID = {id}");
+        SqliteCommand sqlite_cmd = new($"DELETE FROM WorkTime WHERE DATE = {id}");
     }
     public static void DropTable()
     {
-        using var conn = new SqliteConnection(DatabasePath);
+        using var conn = new SqliteConnection(DBpath);
         conn.Open();
         SqliteCommand sqlite_cmd = new("DROP WorkTime");
     }
